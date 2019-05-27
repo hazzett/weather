a1f912c12a34dd7a591a4a580504204dca08bc44
Sam <hazzett@yandex.com>
2019-05-27 14:14:54 +0100

    ready for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // paths for express config
 const publicDirPath = path.join(__dirname, '../public');
@@ -70,6 +71,6 @@ app.get('*', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('port 3000 (Y)');
+app.listen(port, () => {
+    console.log('Serving to port '+port+' (Y)');
 })
