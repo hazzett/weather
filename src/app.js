4bcc38c006c7b7de402a7c4621c473f3142cd9f7
Sam <hazzett@yandex.com>
2019-05-27 14:19:06 +0100

    title rectification

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.use(express.static(publicDirPath));
 app.get('', (req, res) => {
     res.render('index', {
         name: 'sam',
-        title: 'title',
+        title: 'Weather man Sam',
     });
 })
 app.get('/about', (req, res) => {
